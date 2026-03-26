8c4d3b0f4f6a301b7d5386027d25665844ff9385
Laimonas-Mituzas <laimonas.mituzas@gmail.com>
2026-03-26 09:23:15 +0200

    User limited notes. Response.

--- controllers/note.php
+++ controllers/note.php
@@ -11,4 +11,13 @@ $sql = "SELECT * FROM notes WHERE id = :id";
 
 $note = $db->query($sql, ['id' => $_GET["id"]])->fetch();
 
+if (!$note) {
+    abort();
+}
+
+$currentUserId = 1;
+if ($note["user_id"] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
+
 require "views/note.view.php";
---
+++ views/403.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php require "views/partials/head.php"; ?>
+
+<?php require "views/partials/nav.php"; ?>
+
+
+
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      Unauthorized access. You do not have permission to view this page.
+     
+    </div>
+    <a href="/" class="text-blue-500 hover:underline">Go back to Home page</a>
+  </main>
+
+
+<?php require "views/partials/footer.php"; ?>
