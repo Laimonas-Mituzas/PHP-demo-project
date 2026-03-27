b2d67d5f5eb1e8470ad6daa555ac02dc062d5559
Laimonas-Mituzas <laimonas.mituzas@gmail.com>
2026-03-27 09:29:10 +0200

    +create note view, +route

--- views/index.view.php
+++ views/index.view.php
@@ -7,7 +7,13 @@
   
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+
+      <h1 class="text-3xl font-bold text-gray-900">
+        Create Note
+      </h1>
+      
       Welcome to your Home page dashboard!
+      
     </div>
   </main>
 
