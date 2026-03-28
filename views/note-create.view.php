a91548772b307b80dab0b21b435d79dd6f99fc19
Laimonas-Mituzas <laimonas.mituzas@gmail.com>
2026-03-28 08:53:25 +0200

    +server side validation

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -15,7 +15,21 @@
         <div class="col-span-full">
           <label for="about" class="block text-sm/6 font-medium text-gray-900">Body</label>
           <div class="mt-2">
-            <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+            <textarea 
+            id="body" 
+            name="body" 
+            rows="3" 
+            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            placeholder="Write your note here..."
+           
+            >
+            <?= $_POST['body'] ?? '' ?>
+          </textarea>
+
+            <?= isset($errors['body']) ? '<p class="text-sm text-red-500 mt-1">' . $errors['body'] . '</p>' : '' ?>
+
+
+
           </div>
         </div>
 
